76a4908b22384baa22651d4210fdbe2d3216dea7
Divyansh <goyaldivyansh8911@gmail.com>
2026-04-13 01:11:06 +0530

    fix: add price, class, subject fields to Book schema

--- src/models/Book.js
+++ src/models/Book.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    author: { type: String, required: true },
+    title:   { type: String, required: true },
+    author:  { type: String, required: true },
+    price:   { type: Number },
+    class:   { type: String, default: '' },
+    subject: { type: String, default: '' },
+    image:   { type: String, default: '' },
   },
   { timestamps: true }
 );
