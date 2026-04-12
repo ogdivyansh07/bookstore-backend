b1c58aed4026d792bf110011691f11f36a8d73d2
Divyansh <goyaldivyansh8911@gmail.com>
2026-04-13 02:16:16 +0530

    fix: admin login route / auth improvements

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -15,6 +15,9 @@ function getCorsOptions() {
       return callback(null, false);
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200,
   };
 }
 
